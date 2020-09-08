c357562a42b0c45337a4a09678fa4db4fff8d77a
James Barzegar <james.barzegar@gmail.com>
2020-09-08 12:53:38 -0400

    Update bootstrap.ts

--- workspaces/gateway/src/bootstrap.ts
+++ workspaces/gateway/src/bootstrap.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as moduleAlias from 'module-alias'
 
-// Init mapped maths
+// Init mapped paths
 ;['config', 'lib']
   .map(x => [x, path.resolve(__dirname, '../', x)])
   .filter(([, p]) => fs.existsSync(p))
