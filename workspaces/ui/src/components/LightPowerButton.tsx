071f5169f413eb7f386e3653bfe62672d6859f0e
James Barzegar <james.barzegar@gmail.com>
2021-09-18 14:26:18 -0400

    removed ui

--- workspaces/ui/src/components/LightPowerButton.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { Button, ButtonProps } from '@chakra-ui/core'
-
-export type LightPowerButtonProps = { isActive: boolean } & ButtonProps
-export const LightPowerButton = ({
-  isActive,
-  ...props
-}: LightPowerButtonProps) => <Button {...props} variant="outline" />
